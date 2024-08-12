5426abb91f8bbf842ba6ffcd05b7f76240a093e7
rkratee <jainkratee@gmail.com>
2024-08-12 11:50:04 +0530

    Frontend Medium Code

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+
+interface BlogCardProps {
+    id: string,
+    authorName: string,
+    title: string,
+    content: string,
+    publishedDate: string
+}
+export const BlogCard = ({
+    id,
+    authorName, 
+    title, 
+    content, 
+    publishedDate}: BlogCardProps) => {
+
+    return <Link to={`/blog/${id}`}>
+    <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-sm cursor-pointer">
+        <div className="flex">
+            <div className="flex justify-center flex-col">
+                <Avatar name={authorName} size="small"/> 
+            </div>
+            <div className="font-extrathin pl-2">{authorName} </div>
+            <div className="flex justify-center flex-col pl-2"> <Circle /></div>
+            <div className="pl-2 font-thin font-slate-500"> {publishedDate} </div>
+        </div>
+        <div className="text-xl font-semibold pt-2">{title}</div>
+        <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
+        <div className="text-slate-500 text-sm font-thin">{`${Math.ceil(content.length / 100)} minute(s)`}</div>
+    </div>
+    </Link>
+}
+
+export function Circle(){
+    return <div className="h-1 w-1 rounded-full bg-slate-500"></div>
+}
+
+export function Avatar({name, size= "small"}: {name: string, size: "small" | "big"}){
+    return <div className={`relative inline-flex items-center justify-center ${size === "small" ?  "w-6 h-6" : "w-10 h-10"}
+    overflow-hidden bg-slate-700 rounded-full dark:bg-gray-600`}>
+    <span className={`${size === "small" ? "text-sm": "text-md"}font-thin text-xs text-gray-100 dark:text-gray-300`}>{name[0]}</span>
+</div>
+}
---
+++ src/components/Quote.tsx
@@ -0,0 +1,15 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="font-bold text-3xl">
+                    "The customer support I received was exceptional.
+                    The Support team went above and beyond to address my concerns."
+                </div>
+                <div className="max-w-md font-semibold text-xl mt-4">Julies Winfield</div>
+                <div className="max-w-sm font-light text-sm text-slate-400">CEO | ACME corp</div>
+            </div>
+        </div>
+
+    </div>
+}
